ef409d66fc556915b43df0d8310d410303ebea03
minit <dndb4902@naver.com>
2024-12-16 11:19:00 +0900

    feat: reactor

--- src/main/kotlin/com/example/webflux/HelloReactor.kt
+++ src/main/kotlin/com/example/webflux/HelloReactor.kt
@@ -1,9 +1,17 @@
 package com.example.webflux
 
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import java.util.*
 
+
+fun example01() {
+    Mono.just("Hello Reactor")
+        .subscribe { println(it) }
+}
 
 fun main() {
-    Mono.just("Hello Reactor")
+    val sequence = Flux.just("Hello", "Reactor")
+    sequence.map { it.lowercase(Locale.getDefault()) }
         .subscribe { println(it) }
 }
